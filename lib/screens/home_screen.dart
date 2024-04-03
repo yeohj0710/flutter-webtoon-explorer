d6be65299c13809b6674b73e9784b5c83c6b28cb
yeohj0710 <yeohj0710@yonsei.ac.kr>
2024-04-03 19:25:36 +0900

    2404031925 webtoon explorer done

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -24,25 +24,31 @@ class HomeScreen extends StatelessWidget {
         ),
         centerTitle: true,
       ),
-      body: FutureBuilder(
-        future: webtoons,
-        builder: (context, snapshot) {
-          if (snapshot.hasData) {
-            return Column(
-              children: [
-                const SizedBox(
-                  height: 180,
-                ),
-                Expanded(
-                  child: makeList(snapshot),
-                ),
-              ],
-            );
-          }
-          return const Center(
-            child: CircularProgressIndicator(),
-          );
-        },
+      body: Stack(
+        children: [
+          FutureBuilder(
+            future: webtoons,
+            builder: (context, snapshot) {
+              if (snapshot.hasData) {
+                return Column(
+                  children: [
+                    Expanded(
+                      child: makeList(snapshot),
+                    ),
+                  ],
+                );
+              }
+              return const Center(
+                child: CircularProgressIndicator(),
+              );
+            },
+          ),
+          const Positioned(
+            bottom: 40,
+            right: 30,
+            child: RainbowText(),
+          ),
+        ],
       ),
     );
   }
@@ -52,7 +58,7 @@ class HomeScreen extends StatelessWidget {
       scrollDirection: Axis.horizontal,
       itemCount: snapshot.data!.length,
       padding: const EdgeInsets.symmetric(
-        vertical: 10,
+        vertical: 180,
         horizontal: 100,
       ),
       itemBuilder: (context, index) {
@@ -65,6 +71,54 @@ class HomeScreen extends StatelessWidget {
       },
       separatorBuilder: (context, index) => const SizedBox(
         width: 40,
+      ),
+    );
+  }
+}
+
+class RainbowText extends StatefulWidget {
+  const RainbowText({super.key});
+
+  @override
+  _RainbowTextState createState() => _RainbowTextState();
+}
+
+class _RainbowTextState extends State<RainbowText>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<Color?> _animation;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 3),
+    );
+    _animation = ColorTween(
+      begin: Colors.pink,
+      end: Colors.cyan,
+    ).animate(_controller)
+      ..addListener(() {
+        setState(() {});
+      });
+    _controller.repeat(reverse: true);
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      "by yeohj0710",
+      style: TextStyle(
+        fontSize: 18,
+        fontWeight: FontWeight.w600,
+        color: _animation.value,
       ),
     );
   }
